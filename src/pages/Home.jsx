602d2137ad6e81707accbfba640dcc1669ea9db6
Nilay Singh <snilay011@gmail.com>
2023-01-23 14:00:49 +0530

    Passing Images as Props Issue Solved

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,63 @@ import Main from '../components/Main'
 import Navbar from '../components/Navbar'
 import Register from './Register'
 import SignIn from './SignIn'
+import ModelY from '../assets/model-y.jpg'
+import Model3 from '../assets/model-3.jpg'
+import ModelS from '../assets/model-s.jpg'
+import ModelX from '../assets/model-x.jpg'
+import SolarPanel from '../assets/solar-panel.jpg'
+import SolarRoof from '../assets/solar-roof.jpg'
+
+
 
 const Home = () => {
   return (
     <div>
         <Navbar/>
-        {/* <Main
+        <Main
         title = "Model Y"
         text = "Schedule a Demo Drive"
-        bgImg = "../assets/model-y.jpg"
+        bgImg = {ModelY}
         leftBtnText = "Custom Order"
         rightBtnText = "View Inventory"
          />
         <Main
-        title = "Model X"
+        title = "Model 3"
         text = "Schedule a Demo Drive"
-        bgImg = "../assets/model-x.jpg"
+        bgImg = {Model3}
         leftBtnText = "Custom Order"
         rightBtnText = "View Inventory"
          />
         <Main
         title = "Model S"
         text = "Schedule a Demo Drive"
-        bgImg = "../assets/model-s.jpg"
+        bgImg = {ModelS}
         leftBtnText = "Custom Order"
         rightBtnText = "View Inventory"
-         /> */}
+         />
+        <Main
+        title = "Model X"
+        text = "Schedule a Demo Drive"
+        bgImg = {ModelX}
+        leftBtnText = "Custom Order"
+        rightBtnText = "View Inventory"
+         />
+        <Main
+        title = "Solar Panels"
+        text = "Lowest Cost Solar Panels in America"
+        bgImg = {SolarPanel}
+        leftBtnText = "Order Now"
+        rightBtnText = "Learn More"
+         />
+        <Main
+        title = "Solar Roof"
+        text = "Produce Clean Energy From Your Roof"
+        bgImg = {SolarRoof}
+        leftBtnText = "Order Now"
+        rightBtnText = "Learn More"
+         />
          {/* <SignIn/> */} 
-         <Register/>
+         {/* <Register/> */}
     </div>
   )
 }
