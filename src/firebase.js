65257130d1d038943225b77f3b0e5e242a90d49d
Nilay Singh <snilay011@gmail.com>
2023-02-05 01:44:56 +0530

    Firebase keys added to env file

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZYdXfbvnMpHODCw9mUIpIXnVFf5uyPpM",
-  authDomain: "tesla-clone-eb138.firebaseapp.com",
-  projectId: "tesla-clone-eb138",
-  storageBucket: "tesla-clone-eb138.appspot.com",
-  messagingSenderId: "1022705318499",
-  appId: "1:1022705318499:web:bad353e6fb7f9288f59656"
+  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER,
+  appId:process.env.REACT_APP_FIREBASE_API_ID
 };
 
 // Initialize Firebase
